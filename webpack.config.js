7cffdb4c7058d4eacf28a672eea1a2235008813e
Tiffany <tiffany.c.stallings@gmail.com>
2017-07-07 15:28:31 -0400

    Set up webpack config to transform project react js files into compiled js through babel loader.

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,16 @@
-module.exports = {};
+module.exports = {
+  entry: __dirname + '/app/script.js',
+  module: {
+  	loaders: [
+  	   {
+  	   	  test: /\.js$/,
+  	   	  exclude: /node_modules/,
+  	   	  loader: 'babel-loader'
+  	   }
+  	]
+  },
+  output: {
+  	filename: 'final.js',
+  	path: __dirname + '/build'
+  }
+};
