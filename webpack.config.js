e99d32df76062a3214a0d28965711d89fca5f688
Tiffany <tiffany.c.stallings@gmail.com>
2017-07-08 21:12:43 -0400

    Add additional webpack settings to account for json-loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,25 @@ module.exports = {
   	   	  test: /\.js$/,
   	   	  exclude: /node_modules/,
   	   	  loader: 'babel-loader'
-  	   }
+  	   },
+       {
+          test: /\.json$/,
+          exclude: /node_modules/,
+          loader: 'json-loader'
+       }
   	]
+  },
+  resolve: {
+    extensions: ['.webpack.js', '.web.js', '.js']
   },
   output: {
   	filename: 'final.js',
   	path: __dirname + '/build'
+  },
+  node: {
+    console: true,
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
   }
 };
